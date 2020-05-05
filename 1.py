02344e1a886ed853af00d794f09928220a79c8c9
easycodescreator <55099584+easycodescreator@users.noreply.github.com>
2020-05-06 01:32:51 +0430

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+person = 0
+list_person = []
+while person <= 4:
+    name = input("Please tell us your name: ")
+    list_person.append(name)
+    person += 1
+print()   
+print(list_person)
+print("#####################################################################") 
+print("$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%$%")
+Hi = 0
+z = 0
+while Hi <= 4:
+    print(f"Hello {list_person[z]}")
+    z += 1
+    Hi += 1
+    
